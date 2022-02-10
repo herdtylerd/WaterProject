31960d853d44fddb200d5738043048193c1608e6
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-02-10 10:32:30 -0700

    Built project, database, etc. Videos 1-7 of Mission 7

---
+++ WaterProject/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterProject.Models;
+
+namespace WaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Set up the next few lines to connect function
+        private WaterProjectContext context { get; set; }
+
+        public HomeController(WaterProjectContext temp) => context = temp;
+
+        public IActionResult Index()
+        {
+            var blah = context.Projects.ToList();
+
+            return View(blah);
+        }
+    }
+}
--- WaterProject/Startup.cs
+++ WaterProject/Startup.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WaterProject.Models;
 
 namespace WaterProject
 {
@@ -14,8 +17,24 @@ namespace WaterProject
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+
+        // Insert the following lines to set up database
+        public IConfiguration Configuration { get; set; }
+
+        public Startup (IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+        
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews(); // Use mvc pattern
+
+            services.AddDbContext<WaterProjectContext>(options =>
+           {
+               options.UseSqlite(Configuration["ConnectionStrings:WaterDBConnection"]);
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +45,15 @@ namespace WaterProject
                 app.UseDeveloperExceptionPage();
             }
 
+            // Corresponds to wwwroot
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Add endpoint
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
